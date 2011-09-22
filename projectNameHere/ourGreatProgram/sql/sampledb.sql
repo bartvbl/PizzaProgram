4421383fedcaee354ec57e6ca32f4ef2d0a02928
Bart van Blokland <bartiver@stud.ntnu.no>
2011-09-22 10:30:28 +0200

    addedd some address columns to the customer data, created some sample customers

--- projectNameHere/ourGreatProgram/sql/sampledb.sql
+++ projectNameHere/ourGreatProgram/sql/sampledb.sql
@@ -1,8 +1,13 @@
 USE PizzaBase;
 
 #Customer values
-INSERT INTO Customer VALUES (NULL, 'Jack', 'Sparrow', '<Harald Gilles veg 1> 7052 Trondheim Norway', 56729572, NULL);
-INSERT INTO Customer VALUES (NULL, 'The', 'Engineer', 'meship', 56729572, NULL);
-INSERT INTO Customer VALUES (NULL, 'Jack', 'Sparrow', 'meship', 56729572, NULL);
-INSERT INTO Customer VALUES (NULL, 'Jack', 'Sparrow', 'meship', 56729572, NULL);
-INSERT INTO Customer VALUES (NULL, 'Jack', 'Sparrow', 'meship', 56729572, NULL);
+INSERT INTO Customer VALUES (NULL, 'Jack', 'Sparrow', 'Harald Gilles veg', '1', 7052 'Trondheim', 56729572, NULL);
+INSERT INTO Customer VALUES (NULL, 'The', 'Engineer', 'O. S. Bragstads Plass', '2E', 7034 'Trondheim', 38562047, NULL);
+INSERT INTO Customer VALUES (NULL, 'Sherlock', 'Holmes', 'Konrad Dahls Vei', '22', 7024 'Trondheim', 50174506, NULL);
+INSERT INTO Customer VALUES (NULL, 'Commander', 'Shepard', 'Aunevegen', '10', 7021 'Trondheim', 92548936, NULL);
+INSERT INTO Customer VALUES (NULL, 'Dr.', 'Troll', 'Tryms Veg', '8', 7033 'Trondheim', 83670186, NULL);
+INSERT INTO Customer VALUES (NULL, 'The', 'Emperor', 'Ole L. Kolstads veg', '17', 7088 'Trondheim', 20672860, NULL);
+INSERT INTO Customer VALUES (NULL, 'Pepper', 'Roni', 'Paul Røstads veg', '9', 7039 'Trondheim', 60257305, NULL);
+INSERT INTO Customer VALUES (NULL, 'Steve', 'Jobs', 'Sunnlandsvegen', '16', 7032 'Trondheim', 60125183, NULL);
+INSERT INTO Customer VALUES (NULL, 'Wall', 'E', 'Klosterenget', '6', 7030 'Trondheim', 20572919, NULL);
+INSERT INTO Customer VALUES (NULL, 'Gordon', 'Freeman', 'Ada Arnfinsens veg', '18b', 7036 'Trondheim', 89353926, NULL);
