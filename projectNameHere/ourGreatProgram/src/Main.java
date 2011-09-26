8e55839d9b400e6145551d3e28d58a6b34b5eba7
havard <eidheim@live.no>
2011-09-26 21:13:46 +0200

    main er nå online

---
+++ projectNameHere/ourGreatProgram/src/Main.java
@@ -0,0 +1,56 @@
+import java.sql.SQLException;
+
+import pizzaProgram.database.DatabaseConnection;
+import pizzaProgram.events.EventDispatcher;
+import pizzaProgram.gui.ProgramWindow;
+
+/**
+ * The Main class acts as the root of the system. Its main task is to hold references and initialize various parts/modules
+ * @author Bart
+ *
+ */
+public class Main {
+	//
+	/**
+	 * A reference to the main event dispatcher, which represents the communication backbone of the program
+	 */
+	public EventDispatcher eventDispatcher;
+	
+	/**
+	 * A reference to the class that managed the program's main window
+	 */
+	private ProgramWindow programWindow;
+	
+	
+	private DatabaseConnection databaseConnection;
+	
+	/**
+	 * creates every part of the program, and sets it up correctly
+	 */
+	public void initialize()
+	{
+		this.eventDispatcher = new EventDispatcher();
+		this.connectToDatabase();
+		this.createMainWindow();
+		
+	}
+	
+	private void createMainWindow()
+	{
+		this.programWindow = new ProgramWindow(this.eventDispatcher);
+		this.programWindow.createMenuBar();
+		this.programWindow.createMainWindow(640, 480);
+		this.programWindow.createGUIViews(databaseConnection);
+	}
+	
+	private void connectToDatabase()
+	{
+		try {
+			this.databaseConnection = new DatabaseConnection();
+			this.databaseConnection.connect();
+			this.databaseConnection.buildContents();
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+	}
+}
