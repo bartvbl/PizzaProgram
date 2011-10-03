67404efabd741511b8b2571214611937455d54fb
Bart van Blokland <bartvblokl@gmail.com>
2011-10-03 20:40:27 +0200

    added a zip file with the netbeans project containing the order GUI

--- projectNameHere/ourGreatProgram/src/pizzaProgram/gui/table/ReadOnlyJTable.java
+++ projectNameHere/ourGreatProgram/src/pizzaProgram/gui/table/ReadOnlyJTable.java
@@ -3,5 +3,15 @@ package pizzaProgram.gui.table;
 import javax.swing.JTable;
 
 public class ReadOnlyJTable extends JTable {
+	public final ReadOnlyTableModel tableModel;
 	
+	public static ReadOnlyJTable createInstance()
+	{
+		return new ReadOnlyJTable(new ReadOnlyTableModel());
+	}
+	public ReadOnlyJTable(ReadOnlyTableModel model)
+	{
+		super(model);
+		this.tableModel = model;
+	}
 }
