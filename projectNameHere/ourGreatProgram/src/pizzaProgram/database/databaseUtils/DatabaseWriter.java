a9d5a91c0face27339294fe14d2fd4cdc8b1534c
Henning M. Wold <henningwold@gmail.com>
2011-11-17 15:11:03 +0100

    Readded the Identifier field to the Customer table to ensure the uniqueness of customers.

--- projectNameHere/ourGreatProgram/src/pizzaProgram/database/databaseUtils/DatabaseWriter.java
+++ projectNameHere/ourGreatProgram/src/pizzaProgram/database/databaseUtils/DatabaseWriter.java
@@ -14,73 +14,91 @@ import pizzaProgram.dataObjects.UnaddedOrder;
 import pizzaProgram.database.DatabaseConnection;
 
 public class DatabaseWriter {
-	public static void markOrderAsInProgress(Order order){
-		updateOrderStatusIfStatusMatchesCurrentStatus(order, Order.REGISTERED, Order.BEING_COOKED);
+	public static void markOrderAsInProgress(Order order) {
+		updateOrderStatusIfStatusMatchesCurrentStatus(order, Order.REGISTERED,
+				Order.BEING_COOKED);
 	}
-	
-	public static void markOrderAsFinishedCooking(Order order){
-		updateOrderStatusIfStatusMatchesCurrentStatus(order, Order.BEING_COOKED, Order.HAS_BEEN_COOKED);
+
+	public static void markOrderAsFinishedCooking(Order order) {
+		updateOrderStatusIfStatusMatchesCurrentStatus(order,
+				Order.BEING_COOKED, Order.HAS_BEEN_COOKED);
 	}
-	
-	public static void markOrderAsBeingDelivered(Order order){
-		if(order.deliveryMethod.equals(Order.DELIVER_AT_HOME)){
-			updateOrderStatusIfStatusMatchesCurrentStatus(order, Order.HAS_BEEN_COOKED, Order.BEING_DELIVERED);
-		}
-		else {
-			DatabaseResultsFeedbackProvider.showUpdateOrderStatusFailedInvalidDeliveryMethodMessage();
-		}
-	}
-	
-	public static void markOrderAsDelivered(Order order){
-		if(order.deliveryMethod.equals(Order.DELIVER_AT_HOME)){
-			updateOrderStatusIfStatusMatchesCurrentStatus(order, Order.BEING_DELIVERED, Order.DELIVERED);
-		}
-		else if(order.deliveryMethod.equals(Order.PICKUP_AT_RESTAURANT)){
-			updateOrderStatusIfStatusMatchesCurrentStatus(order, Order.HAS_BEEN_COOKED, Order.DELIVERED);
+
+	public static void markOrderAsBeingDelivered(Order order) {
+		if (order.deliveryMethod.equals(Order.DELIVER_AT_HOME)) {
+			updateOrderStatusIfStatusMatchesCurrentStatus(order,
+					Order.HAS_BEEN_COOKED, Order.BEING_DELIVERED);
+		} else {
+			DatabaseResultsFeedbackProvider
+					.showUpdateOrderStatusFailedInvalidDeliveryMethodMessage();
 		}
 	}
-	
-	public static void writeNewOrder(UnaddedOrder order){
-		try{
+
+	public static void markOrderAsDelivered(Order order) {
+		if (order.deliveryMethod.equals(Order.DELIVER_AT_HOME)) {
+			updateOrderStatusIfStatusMatchesCurrentStatus(order,
+					Order.BEING_DELIVERED, Order.DELIVERED);
+		} else if (order.deliveryMethod.equals(Order.PICKUP_AT_RESTAURANT)) {
+			updateOrderStatusIfStatusMatchesCurrentStatus(order,
+					Order.HAS_BEEN_COOKED, Order.DELIVERED);
+		}
+	}
+
+	public static void writeNewOrder(UnaddedOrder order) {
+		try {
 			int commentID = -1;
-			if(order.comment != ""){
+			if (order.comment != "") {
 				commentID = createOrderComment(order.comment);
 			}
 			int orderID = createNewOrder(order, commentID);
-			if(orderID != -1){
+			if (orderID != -1) {
 				addDishesToOrder(order, orderID);
 			}
 			DatabaseResultsFeedbackProvider.showAddNewOrderSuccessMessage();
-		}
-		catch(SQLException e){
+		} catch (SQLException e) {
 			DatabaseResultsFeedbackProvider.showAddNewOrderFailedMessage();
 		}
 	}
-	
+
 	public static void writeNewCustomer(UnaddedCustomer customer) {
 		int commentID;
 		try {
 			commentID = createCustomerNote(customer.comment);
-			DatabaseConnection.executeWriteQuery("INSERT INTO Customer VALUES (NULL, '"+customer.firstName+"', '"+customer.lastName+"', '"+customer.address+"', "+customer.postalCode+", '"+customer.city+"', "+customer.phoneNumber+", "+commentID+");");
+			DatabaseConnection
+					.executeWriteQuery("INSERT INTO Customer VALUES (NULL, '"
+							+ customer.firstName + "', '" + customer.lastName
+							+ "', '" + customer.address + "', "
+							+ customer.postalCode + ", '" + customer.city
+							+ "', " + customer.phoneNumber + ", " + commentID
+							+ ", '" + createCustomerIdentifier(customer)
+							+ "');");
 			DatabaseResultsFeedbackProvider.showAddNewCustomerSuccessMessage();
 		} catch (SQLException e) {
 			DatabaseResultsFeedbackProvider.showAddNewCustomerFailedMessage();
 			e.printStackTrace();
 		}
 	}
-	public static void updateCustomerById(Customer customer){
+
+	public static void updateCustomerById(Customer customer) {
 		int commentID;
 		try {
 			commentID = createCustomerNote(customer.comment);
-			DatabaseConnection.executeWriteQuery("REPLACE INTO Customer VALUES (" + customer.customerID + ", '"+customer.firstName+"', '"+customer.lastName+"', '"+customer.address+"', "+customer.postalCode+", '"+customer.city+"', "+customer.phoneNumber+", "+commentID+");");
+			DatabaseConnection
+					.executeWriteQuery("REPLACE INTO Customer VALUES ("
+							+ customer.customerID + ", '" + customer.firstName
+							+ "', '" + customer.lastName + "', '"
+							+ customer.address + "', " + customer.postalCode
+							+ ", '" + customer.city + "', "
+							+ customer.phoneNumber + ", " + commentID + ", '"
+							+ createCustomerIdentifier(customer) + "');");
 			DatabaseResultsFeedbackProvider.showAddNewCustomerSuccessMessage();
 		} catch (SQLException e) {
 			DatabaseResultsFeedbackProvider.showAddNewCustomerFailedMessage();
 			e.printStackTrace();
 		}
 	}
-	
-	public static void writeNewDish(Dish dish){
+
+	public static void writeNewDish(Dish dish) {
 		int containsGluten = convertBooleanToTinyInt(dish.containsGluten);
 		int containsNuts = convertBooleanToTinyInt(dish.containsNuts);
 		int containsDairy = convertBooleanToTinyInt(dish.containsDiary);
@@ -88,15 +106,21 @@ public class DatabaseWriter {
 		int isVegetarian = convertBooleanToTinyInt(dish.isVegetarian);
 		int dishIsActive = convertBooleanToTinyInt(dish.isActive);
 		try {
-			DatabaseConnection.executeWriteQuery("INSERT INTO Dishes VALUES (NULL, "+dish.price+", '"+dish.name+"', "+containsGluten+", "+containsNuts+", "+containsDairy+", "+isSpicy+", "+isVegetarian+", '"+dish.description+"', "+dishIsActive+");");
+			DatabaseConnection
+					.executeWriteQuery("INSERT INTO Dishes VALUES (NULL, "
+							+ dish.price + ", '" + dish.name + "', "
+							+ containsGluten + ", " + containsNuts + ", "
+							+ containsDairy + ", " + isSpicy + ", "
+							+ isVegetarian + ", '" + dish.description + "', "
+							+ dishIsActive + ");");
 			DatabaseResultsFeedbackProvider.showAddNewDishSucceededMessage();
 		} catch (SQLException e) {
 			DatabaseResultsFeedbackProvider.showAddNewDishFailedMessage();
 			e.printStackTrace();
 		}
 	}
-	
-	public static void updateDishByDishID(Dish dish){
+
+	public static void updateDishByDishID(Dish dish) {
 		try {
 			int containsGluten = convertBooleanToTinyInt(dish.containsGluten);
 			int containsDairy = convertBooleanToTinyInt(dish.containsDiary);
@@ -104,127 +128,168 @@ public class DatabaseWriter {
 			int isVegetarian = convertBooleanToTinyInt(dish.isVegetarian);
 			int isSpicy = convertBooleanToTinyInt(dish.isSpicy);
 			int isActive = convertBooleanToTinyInt(dish.isActive);
-			DatabaseConnection.executeWriteQuery("UPDATE Dishes SET Price="+dish.price+", Name='"+dish.name+"', ContainsGluten="+containsGluten+", ContainsNuts="+containsNuts+", ContainsDairy="+containsDairy+", IsSpicy="+isSpicy+", IsVegetarian="+isVegetarian+", Description='"+dish.description+"', IsActive="+isActive+" WHERE DishID="+dish.dishID+";");
+			DatabaseConnection.executeWriteQuery("UPDATE Dishes SET Price="
+					+ dish.price + ", Name='" + dish.name
+					+ "', ContainsGluten=" + containsGluten + ", ContainsNuts="
+					+ containsNuts + ", ContainsDairy=" + containsDairy
+					+ ", IsSpicy=" + isSpicy + ", IsVegetarian=" + isVegetarian
+					+ ", Description='" + dish.description + "', IsActive="
+					+ isActive + " WHERE DishID=" + dish.dishID + ";");
 		} catch (SQLException e) {
 			DatabaseResultsFeedbackProvider.showUpdateDishFailedMessage();
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void updateExtraByExtraID(Extra extra) {
 		try {
 			int isActive = convertBooleanToTinyInt(extra.isActive);
-			String price = extra.priceFuncPart + Double.toString(extra.priceValPart);
-			DatabaseConnection.executeWriteQuery("UPDATE Extras SET Name='"+extra.name+"', Price='"+price+"', IsActive="+isActive+" WHERE ExtrasID="+extra.id+";");
+			String price = extra.priceFuncPart
+					+ Double.toString(extra.priceValPart);
+			DatabaseConnection.executeWriteQuery("UPDATE Extras SET Name='"
+					+ extra.name + "', Price='" + price + "', IsActive="
+					+ isActive + " WHERE ExtrasID=" + extra.id + ";");
 		} catch (SQLException e) {
 			DatabaseResultsFeedbackProvider.showUpdateExtraFailedMessage();
 			e.printStackTrace();
 		}
 	}
-	
-	public static void writeNewExtra(Extra extra){
+
+	public static void writeNewExtra(Extra extra) {
 		int extraIsActive = convertBooleanToTinyInt(extra.isActive);
-		String priceString = extra.priceFuncPart + Double.toString(extra.priceValPart);
+		String priceString = extra.priceFuncPart
+				+ Double.toString(extra.priceValPart);
 		try {
-			DatabaseConnection.executeWriteQuery("INSERT INTO Extras VALUES (NULL, '"+extra.name+"', '"+priceString+"', "+extraIsActive+");");
+			DatabaseConnection
+					.executeWriteQuery("INSERT INTO Extras VALUES (NULL, '"
+							+ extra.name + "', '" + priceString + "', "
+							+ extraIsActive + ");");
 			DatabaseResultsFeedbackProvider.showAddNewExtraSucceededMessage();
 		} catch (SQLException e) {
 			DatabaseResultsFeedbackProvider.showAddNewExtraFailedMessage();
 			e.printStackTrace();
 		}
 	}
-	
-	public static void updateConfigValue(Setting setting){
+
+	public static void updateConfigValue(Setting setting) {
 		try {
-			DatabaseConnection.executeWriteQuery("UPDATE Config SET ConfigValue='"+setting.value+"' WHERE ConfigKey='"+setting.key+"'");
+			DatabaseConnection
+					.executeWriteQuery("UPDATE Config SET ConfigValue='"
+							+ setting.value + "' WHERE ConfigKey='"
+							+ setting.key + "'");
 		} catch (SQLException e) {
-			DatabaseResultsFeedbackProvider.showUpdateConfigValueFailedMessage();
+			DatabaseResultsFeedbackProvider
+					.showUpdateConfigValueFailedMessage();
 			e.printStackTrace();
 		}
 	}
-	
-	private static void updateOrderStatusIfStatusMatchesCurrentStatus(Order order, String currentStatus, String newStatus){
+
+	private static void updateOrderStatusIfStatusMatchesCurrentStatus(
+			Order order, String currentStatus, String newStatus) {
 		try {
 			lockTablesForUpdatingOrderStatus();
 			String currentOrderStatus = getCurrentStatusOfOrder(order.orderID);
-			if(currentOrderStatus.equals(currentStatus)){
+			if (currentOrderStatus.equals(currentStatus)) {
 				updateOrderStatus(newStatus, order.orderID);
 				unlockTables();
-			}
-			else {
+			} else {
 				unlockTables();
-				DatabaseResultsFeedbackProvider.showUpdateOrderStatusFailedMessage();
+				DatabaseResultsFeedbackProvider
+						.showUpdateOrderStatusFailedMessage();
 			}
 		} catch (SQLException e) {
-			
+
 			e.printStackTrace();
-		} finally{
+		} finally {
 			unlockTables();
 		}
 	}
-	
-	private static String getCurrentStatusOfOrder(int orderID) throws SQLException{
-		String query = "SELECT OrdersStatus FROM Orders AS OrdersRead WHERE OrdersID="+orderID+";";
+
+	private static String getCurrentStatusOfOrder(int orderID)
+			throws SQLException {
+		String query = "SELECT OrdersStatus FROM Orders AS OrdersRead WHERE OrdersID="
+				+ orderID + ";";
 		ResultSet results = DatabaseConnection.fetchData(query);
 		results.next();
 		String currentStatus = results.getString(1);
 		return currentStatus;
 	}
 
-	private static void updateOrderStatus(String status, int orderID) throws SQLException{
-		DatabaseConnection.executeWriteQuery("UPDATE Orders SET OrdersStatus='"+status+"' WHERE OrdersID="+orderID+";");
+	private static void updateOrderStatus(String status, int orderID)
+			throws SQLException {
+		DatabaseConnection.executeWriteQuery("UPDATE Orders SET OrdersStatus='"
+				+ status + "' WHERE OrdersID=" + orderID + ";");
 	}
-	
-	private static void lockTablesForUpdatingOrderStatus() throws SQLException{
-		DatabaseConnection.executeWriteQuery("LOCK TABLES Orders WRITE, Orders AS OrdersRead READ;");
+
+	private static void lockTablesForUpdatingOrderStatus() throws SQLException {
+		DatabaseConnection
+				.executeWriteQuery("LOCK TABLES Orders WRITE, Orders AS OrdersRead READ;");
 	}
-	
-	private static void unlockTables(){
+
+	private static void unlockTables() {
 		DatabaseConnection.insertIntoDB("UNLOCK TABLES;");
 	}
 
-	private static void addDishesToOrder(UnaddedOrder order, int orderID) throws SQLException {
-		for(OrderDish dish : order.orderedDishes){
+	private static void addDishesToOrder(UnaddedOrder order, int orderID)
+			throws SQLException {
+		for (OrderDish dish : order.orderedDishes) {
 			addDishToOrder(dish, orderID);
 		}
 	}
 
-	private static void addDishToOrder(OrderDish dish, int orderID) throws SQLException {
-		int orderContentsID = DatabaseConnection.insertIntoDBAndReturnID("INSERT INTO OrdersContents VALUES (NULL, "+orderID+", "+dish.dish.dishID+");");
-		if(orderContentsID == -1){
+	private static void addDishToOrder(OrderDish dish, int orderID)
+			throws SQLException {
+		int orderContentsID = DatabaseConnection
+				.insertIntoDBAndReturnID("INSERT INTO OrdersContents VALUES (NULL, "
+						+ orderID + ", " + dish.dish.dishID + ");");
+		if (orderContentsID == -1) {
 			return;
 		}
-		for(Extra extra : dish.getExtras()){
+		for (Extra extra : dish.getExtras()) {
 			addExtraToOrder(orderContentsID, extra);
 		}
 	}
 
 	private static void addExtraToOrder(int orderContentsID, Extra extra) {
-		DatabaseConnection.insertIntoDB("INSERT INTO DishExtrasChosen VALUES ("+orderContentsID+", "+extra.id+")");
+		DatabaseConnection.insertIntoDB("INSERT INTO DishExtrasChosen VALUES ("
+				+ orderContentsID + ", " + extra.id + ")");
 	}
 
 	private static int createOrderComment(String comment) throws SQLException {
-		int commentID = DatabaseConnection.insertIntoDBAndReturnID("INSERT INTO OrderComments VALUES (NULL, '"+comment+"');");
+		int commentID = DatabaseConnection
+				.insertIntoDBAndReturnID("INSERT INTO OrderComments VALUES (NULL, '"
+						+ comment + "');");
 		return commentID;
 	}
 
-	private static int createNewOrder(UnaddedOrder order, int commentID) throws SQLException {
-		int orderID = DatabaseConnection.insertIntoDBAndReturnID("INSERT INTO Orders VALUES (NULL, "+order.customer.customerID+", NOW(), '"+Order.REGISTERED+"', '"+order.deliveryMethod+"', "+commentID+");");
+	private static int createNewOrder(UnaddedOrder order, int commentID)
+			throws SQLException {
+		int orderID = DatabaseConnection
+				.insertIntoDBAndReturnID("INSERT INTO Orders VALUES (NULL, "
+						+ order.customer.customerID + ", NOW(), '"
+						+ Order.REGISTERED + "', '" + order.deliveryMethod
+						+ "', " + commentID + ");");
 		return orderID;
 	}
-	
+
 	private static int createCustomerNote(String comment) throws SQLException {
-		int commentID = DatabaseConnection.insertIntoDBAndReturnID("INSERT INTO OrderComments VALUES (NULL, '"+comment+"');");
+		int commentID = DatabaseConnection
+				.insertIntoDBAndReturnID("INSERT INTO OrderComments VALUES (NULL, '"
+						+ comment + "');");
 		return commentID;
 	}
-	
-	private static int convertBooleanToTinyInt(boolean bool){
-		if(bool == true){
+
+	private static int convertBooleanToTinyInt(boolean bool) {
+		if (bool == true) {
 			return 1;
-		} 
-		else {
+		} else {
 			return 0;
 		}
 	}
-	
-}//END
+
+	public static String createCustomerIdentifier(Customer customer) {
+		return customer.firstName.toLowerCase()
+				+ customer.lastName.toLowerCase() + customer.phoneNumber;
+	}
+
+}// END
