4cd3bdd87101e34113e46a50109c3e73e30ac44d
havard <eidheim@live.no>
2011-11-18 17:03:19 +0100

    fixed a little problem

--- projectNameHere/ourGreatProgram/src/pizzaProgram/gui/views/ReceiptWindow.java
+++ projectNameHere/ourGreatProgram/src/pizzaProgram/gui/views/ReceiptWindow.java
@@ -1,9 +1,12 @@
 package pizzaProgram.gui.views;
 
+import javax.print.DocFlavor;
+import javax.print.DocPrintJob;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 import pizzaProgram.core.Constants;
+import pizzaProgram.utils.PrintUtilities;
 
 @SuppressWarnings("serial")
 public class ReceiptWindow extends JFrame{
@@ -22,16 +25,21 @@ public class ReceiptWindow extends JFrame{
 		this.setLocation(100, 100);
 		this.setResizable(false);
 		this.setTitle("Kvittering");
-		
+
 		html = new JLabel(kvittering, JLabel.LEFT);
 		html.setBounds(0, 5, Constants.RECIPT_WIDTH, rows * Constants.RECIPT_ROW_HEIGHT);
 		html.setVerticalAlignment(JLabel.TOP);
 		html.setHorizontalTextPosition(JLabel.LEFT);
 		html.setVerticalTextPosition(JLabel.TOP);
-		
+
 		this.add(html);
 		this.setSize(html.getWidth() + 10, html.getHeight() + 40);//40 is the height of the framedecoration 10 is the width
 		this.setVisible(true);
+
+		
+		//PrintUtilities pu = new PrintUtilities(html);
+		//pu.print();
+		
 	}
 
 }//END
