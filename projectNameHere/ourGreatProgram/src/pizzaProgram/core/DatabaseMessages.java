6c69e64f7044ad97b2c30ecbff046b4a538e223c
Henning M. Wold <henningwold@gmail.com>
2011-11-22 16:36:37 +0100

    Forgot to add the constants file

---
+++ projectNameHere/ourGreatProgram/src/pizzaProgram/core/DatabaseMessages.java
@@ -0,0 +1,35 @@
+package pizzaProgram.core;
+
+/**
+ * Class containing String constants containing the contents of information and
+ * error messageboxes spawned by the databaseclasses 
+ */
+public class DatabaseMessages {
+	public static final String UNABLE_TO_FETCH_CUSTOMERS = "Kunne ikke hente kunder fra databasen!";
+	public static final String UNABLE_TO_ADD_CUSTOMER = "Kunne ikke legge til kunde!";
+	public static final String CUSTOMER_ADDED_SUCCESSFULLY = "Kunden ble lagt til.";
+	public static final String UNABLE_TO_EDIT_CUSTOMER = "Kunne ikke endre kundedata!";
+	public static final String CUSTOMER_EDITED_SUCCESSFULLY = "Kundedata endret.";
+	
+	public static final String UNABLE_TO_FETCH_SETTINGS = "Kunne ikke hente innstillinger fra databasen!";
+	public static final String UNABLE_TO_CHANGE_BLANK = "Kunne ikke endre ";
+	
+	public static final String UNABLE_TO_FETCH_DISHES = "Kunne ikke hente retter fra databasen!";
+	public static final String UNABLE_TO_ADD_DISH = "Kunne ikke legge til rett!";
+	public static final String DISH_ADDED_SUCCESSFULLY = "Rett lagt til.";
+	public static final String UNABLE_TO_EDIT_DISH = "Kunne ikke endre rett!";
+	public static final String DISH_EDITED_SUCCESSFULLY = "Rett endret.";
+	
+	public static final String UNABLE_TO_FETCH_EXTRAS = "Kunne ikke hente tilbehør fra databasen!";
+	public static final String UNABLE_TO_ADD_EXTRA = "Kunne ikke legge til tilbehør!";
+	public static final String EXTRA_ADDED_SUCCESSFULLY = "Tilbehør lagt til.";
+	public static final String UNABLE_TO_EDIT_EXTRA = "Kunne ikke endre tilbehør!";
+	public static final String EXTRA_EDITED_SUCCESSFULLY = "Tilbehør endret.";
+	
+	public static final String UNABLE_TO_FETCH_ORDERS = "Kunne ikke hente ordre fra databasen!";
+	public static final String UNABLE_TO_ADD_ORDER = "Kunne ikke legge til ordren!";
+	public static final String ORDER_ADDED_SUCCESSFULLY = "Ordren ble lagt til.";
+	public static final String UNABLE_TO_CHANGE_ORDER_STATUS = "Kunne ikke endre ordrestatus.";
+	public static final String ILLEGAL_STATUS_ON_ORDER = "Orderen har en ugyldig status!";
+	public static final String UNEXPECTED_ORDER_STATUS = "Kunne ikke endre ordrestatus: uforventet ordrestatus.";
+}
